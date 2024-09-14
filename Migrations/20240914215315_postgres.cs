10a3f0e72656402e3b3f8d1e82ad49fe9990e177
DiogoWebber <diogoevaldt17@gmail.com>
2024-09-14 18:58:11 -0300

    user

---
+++ Migrations/20240914215315_postgres.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PubgAPI.Migrations
+{
+    public partial class postgres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
